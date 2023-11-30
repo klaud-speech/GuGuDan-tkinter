09102baae8e35174fcc6a24d8013f9f5b02913cb
sahngwoon_lee <sahngwoon.lee@llsollu.com>
2023-11-30 17:03:55 +0900

    BubbleSorting::Reverse()

--- BubbleSorting.py
+++ BubbleSorting.py
@@ -1,7 +1,26 @@
 import numpy as np
 
-random = True;
+def BubbleSorting( arr, descending = False ):
+    # acending
+    for i in range( len(arr)-1, 0, -1):    # number of pairs.    
+        for j in range(i):
+            print( "i=", i, "j=", j )
+            #if arr[j] > arr[j+1]:           # comparision 
+            #if arr[j] < arr[j+1]:           # comparision 
+            comp = arr[j] > arr[j+1]
+            if( (descending==False and comp==True ) or ( descending==True and comp==False  )):
+                arr[j], arr[j+1]   = arr[j+1], arr[j]   #swap 
+            print(" Inner Result: ", arr )
+        
+        print(" >> Partial Result: ", arr )
 
+    print(">>>> Final Result: ", arr )
+    return arr
+
+###############################
+
+
+random = True;
 if( random == False ):
     arr = [40, 70, 60, 30, 10, 50]
 else:
@@ -12,25 +31,8 @@ else:
 
 print("INPUT:  ", arr )
 
+result = BubbleSorting( arr, descending = False )
+print(":::::::::: RESULT: ", result)
 
-
-def BubbleSorting( data ):
-    # acending
-    for i in range( len(arr)-1, 0, -1):    # number of pairs.    
-        for j in range(i):
-            print( "i=", i, "j=", j )
-            if arr[j] > arr[j+1]:           # comparision 
-                arr[j], arr[j+1]   = arr[j+1], arr[j]   #swap 
-            print(" Inner Result: ", arr )
-        
-        print(" >> Partial Result: ", arr )
-
-    print(">>>> Final Result: ", arr )
-    return arr
-
-
-
-result = BubbleSorting( arr)
-print("\n\nResult :",  result )
 
 
