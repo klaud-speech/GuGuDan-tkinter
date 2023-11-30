10da8602b45456d0eb2fdcc128c20f8e68adf226
sahngwoon_lee <sahngwoon.lee@llsollu.com>
2023-11-30 11:25:56 +0900

    Insertion Sorting with While() and only one variable

--- InsertionSorting.py
+++ InsertionSorting.py
@@ -1,5 +1,6 @@
 arr=[40,60,70,50,10,20,30]
 
+# double for문,  with i, j variables
 for i in range(1,len(arr)):
     for j in range(i, 0, -1):
         print("i 번째", i, ", j 인덱스 ", j )
@@ -9,4 +10,18 @@ for i in range(1,len(arr)):
     print(">>Partial Result: ", arr )
 
 print(">>>> Final Result ", arr )
+
+
+# while문 and without j variable
+print("\n\n while문 사용")
+arr=[40,60,70,50,10,20,30]
+for i in range(1,len(arr)):
+    while i>0 and arr[i-1] > arr[i]:
+        arr[i-1], arr[i] = arr[i], arr[i-1]
+        i -= 1
+
+print(arr)
+
+
+
 
