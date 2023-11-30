553e182d63f7450d9006a5189599b74d4c20f2e6
sahngwoon_lee <sahngwoon.lee@llsollu.com>
2023-11-30 16:38:34 +0900

    InsertionSorting::descending:False

--- InsertionSorting.py
+++ InsertionSorting.py
@@ -1,19 +1,6 @@
 import numpy as np
 
-random = True;
-
-if( random == False ):
-    arr=[40,60,70,50,10,20,30]
-else:
-    arr =[]
-    for i in range(10):
-        num1 = np.random.randint(100);
-        arr.append(num1)
-print("INPUT:  ", arr ) 
-
-ascending = False;
-
-def insertionSorting( arr, ascending ):
+def insertionSorting( arr, descending ):
     # double for문,  with i, j variables
     for i in range(1,len(arr)):
         for j in range(i, 0, -1):
@@ -24,7 +11,7 @@ def insertionSorting( arr, ascending ):
             
             #if arr[j-1] > arr[j]:
             ##if arr[j-1] < arr[j]:
-            if( (ascending==True and comp==True ) or ( ascending==False and comp==False  )):
+            if( (descending==False and comp==True ) or ( descending==True and comp==False  )):
                 arr[j], arr[j-1] = arr[j-1], arr[j]
 
             print("Inner Result: ", arr )
@@ -44,16 +31,28 @@ def insertionSorting( arr, ascending ):
         #while i>0 and arr[i-1] > arr[i]:
         while i>0:
             comp = arr[i-1] > arr[i]            
-            if( (ascending==True and comp==True ) or ( ascending==False and comp==False  )):
+            if( (descending==False and comp==True ) or ( descending==True and comp==False  )):
                 arr[i-1], arr[i] = arr[i], arr[i-1]
             i -= 1
 
-    print(arr)
+    print(">>>> FinalResult : " , arr)
     return arr
 
 
 
-result = insertionSorting(arr, True)
-print("RESULT: ", result)
+
+random = True;
+if( random == False ):
+    arr=[40,60,70,50,10,20,30]
+else:
+    arr =[]
+    for i in range(10):
+        num1 = np.random.randint(100);
+        arr.append(num1)
+print("INPUT:  ", arr ) 
+
+
+result = insertionSorting(arr, descending=False)
+print(":::::::::: RESULT: ", result)
 
 
